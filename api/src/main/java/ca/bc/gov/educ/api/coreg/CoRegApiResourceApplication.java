8e491bc9c93b69f3923e550c3f62827304677626
QSL\SumathiT <sumathi.thirumani@quartech.com>
2025-04-11 16:47:39 -0700

    Changes to swagger and open-api path with path /api - revert (for discussion)

--- api/src/main/java/ca/bc/gov/educ/api/coreg/CoRegApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/coreg/CoRegApiResourceApplication.java
@@ -75,12 +75,12 @@ public class CoRegApiResourceApplication {
       http
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
-          .requestMatchers("/v1/api-docs-ui.html",
-                  "/v1/api-docs/**",
+          .requestMatchers("/api/v1/api-docs-ui.html",
+                  "/api/v1/api-docs/**",
                   "/actuator/health",
                   "/actuator/prometheus",
                   "/actuator/**",
-                  "/v1/swagger-ui/**"
+                  "/api/v1/swagger-ui/**"
           ).permitAll()
           .anyRequest().authenticated()
         )
